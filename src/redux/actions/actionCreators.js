5bfdcadf3269180168a0b2eaa51c07fc26407c50
andrii2706 <83228054+andrii2706@users.noreply.github.com>
2021-08-30 14:37:45 +0300

    100%

--- src/redux/actions/actionCreators.js
+++
@@ -1,8 +0,0 @@
-import * as actinTypes from './actionTypes'
-
-export const getAllCharacters = (payload)=>{
-    return {type:actinTypes.GET_ALL_CHARACTERS,payload}
-}
-export const changePagination = (payload)=>{
-    return {type:actinTypes.CHANGE_PAGINATION, payload}
-}
